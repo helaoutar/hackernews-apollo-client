0c0677ba4acb31d23b5259d274e862d84ed1362d
helaoutar <helaoutar@sqli.com>
2019-05-20 11:26:57 +0100

    Authentification done

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,25 @@
 import React, { useState } from "react";
 import { AUTH_TOKEN } from "../../constants";
+import { Mutation } from "react-apollo";
+import gql from "graphql-tag";
 
-const Login = () => {
+const LOGIN_MUTATION = gql`
+  mutation loginMutation($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+const SIGN_UP_MUTATION = gql`
+  mutation signupMutation($name: String!, $email: String!, $password: String!) {
+    signup(name: $name, email: $email, password: $password) {
+      token
+    }
+  }
+`;
+
+const Login = ({ history }) => {
   const [login, setLogin] = useState(true);
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
@@ -9,6 +27,12 @@ const Login = () => {
 
   const _saveUserData = token => {
     localStorage.setItem(AUTH_TOKEN, token);
+  };
+
+  const _confirm = data => {
+    const { token } = data.login || data.signup;
+    _saveUserData(token);
+    history.push("/");
   };
 
   return (
@@ -37,12 +61,21 @@ const Login = () => {
         />
       </div>
       <div className="flex mt3">
-        <div
-          className="pointer mr2 button"
-          onClick={() => console.log("button clicked")}
+        <Mutation
+          mutation={login ? LOGIN_MUTATION : SIGN_UP_MUTATION}
+          variables={{
+            email,
+            password,
+            name
+          }}
+          onCompleted={_confirm}
         >
-          {login ? "login" : "create account"}
-        </div>
+          {postMutation => (
+            <div className="pointer mr2 button" onClick={postMutation}>
+              {login ? "login" : "create account"}
+            </div>
+          )}
+        </Mutation>
         <div className="pointer button" onClick={() => setLogin(!login)}>
           {login ? "need to create an account?" : "already have an account?"}
         </div>
--- src/index.js
+++ src/index.js
@@ -7,16 +7,28 @@ import { ApolloProvider } from "react-apollo";
 import { ApolloClient } from "apollo-client";
 import { createHttpLink } from "apollo-link-http";
 import { InMemoryCache } from "apollo-cache-inmemory";
+import { setContext } from "apollo-link-context";
 
 import "./index.css";
 import App from "./components/App";
+import { AUTH_TOKEN } from "./constants";
 
-const link = createHttpLink({
+const httpLink = createHttpLink({
   uri: "http://localhost:4000"
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  };
+});
+
 const client = new ApolloClient({
-  link,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 });
 
